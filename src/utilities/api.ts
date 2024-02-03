bf557cfed89b3b27f29778ecc02f955ea9569e41
chenAmrani <chenamrani5@gmail.com>
2024-02-03 14:17:18 +0200

    add api.ts

---
+++ src/utilities/api.ts
@@ -0,0 +1,47 @@
+import { ACCESS_TOKEN_KEY, BASE_URL } from "../constants";
+
+const getReviewsByBookId = async (bookId: string) => {
+  const response = await fetch(`${BASE_URL}/review/book/${bookId}`);
+  return response;
+};
+
+const addNewComment = async (bookId: string, text: string) => {
+  const response = await fetch(`${BASE_URL}/review`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem(ACCESS_TOKEN_KEY)}`,
+    },
+    body: JSON.stringify({ bookId, text }),
+  });
+  return response;
+};
+
+const updateReview = async (reviewId: string, text: string) => {
+  const response = await fetch(`${BASE_URL}/review`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem(ACCESS_TOKEN_KEY)}`,
+    },
+    body: JSON.stringify({ text, id: reviewId }),
+  });
+  return response;
+};
+
+const updateUserProfile = async (formData: FormData) => {
+  return await fetch(`${BASE_URL}/user/updateOwnProfile`, {
+    method: "PUT",
+    body: formData,
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem(ACCESS_TOKEN_KEY)}`,
+    },
+  });
+};
+
+export const api = {
+  addNewComment,
+  getReviewsByBookId,
+  updateReview,
+  updateUserProfile,
+};
