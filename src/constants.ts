002cc65b9e2753eeaafbc710a4f834ae057a40fc
chenAmrani <chenamrani5@gmail.com>
2024-02-06 10:43:28 +0200

    add refreshToken

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
 export const BASE_URL = "http://localhost:6969";
 export const STATIC_ASSETS_URL = BASE_URL + "/static/uploads/";
 export const ACCESS_TOKEN_KEY = "accessToken";
+export const REFRESH_TOKEN_KEY = "refreshToken";
