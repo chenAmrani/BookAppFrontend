807122d6de7e80eb620140c4a194cc69278fe182
chenAmrani <chenamrani5@gmail.com>
2024-03-12 14:00:34 +0200

    update constats

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,4 @@
-// export const BASE_URL = process.env.NODE_ENV === 'production' ? "https://193.106.55.216": "http://localhost:6969";
-export const BASE_URL = process.env.NODE_ENV ="https://193.106.55.216";
+export const BASE_URL = process.env.NODE_ENV === 'production' ? "https://193.106.55.216": "http://localhost:6969";
 export const STATIC_ASSETS_URL = BASE_URL + "/static/uploads/";
 export const ACCESS_TOKEN_KEY = "accessToken";
 export const REFRESH_TOKEN_KEY = "refreshToken";
